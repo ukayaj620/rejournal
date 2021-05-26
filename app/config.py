9e2a573480e3338c4be28e57c1c8de65e21856d2
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-05-26 21:32:32 +0700

    chore: update config file

--- app/config.py
+++ app/config.py
@@ -5,12 +5,17 @@ class Config:
     DEBUG = False
     SECRET_KEY = str(os.environ.get('SECRET_KEY'))
     SQLALCHEMY_DATABASE_URI = str(os.environ.get('DATABASE_URL'))
+    ADMIN_PASSWORD = str(os.environ.get('ADMIN_PASSWORD'))
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    UPLOAD_EXTENSIONS = list(str(os.environ.get('UPLOAD_EXTENTIONS')).split(' '))
-    UPLOAD_PATH = str(os.environ.get('UPLOAD_IMAGE_PATH'))
-    UPLOAD_DIR = str(os.environ.get('UPLOAD_IMAGE_DIR'))
+    UPLOAD_IMAGE_EXTENSIONS = list(str(os.environ.get('UPLOAD_IMAGE_EXTENSIONS')).split(' '))
+    UPLOAD_IMAGE_PATH = str(os.environ.get('UPLOAD_IMAGE_PATH'))
+    UPLOAD_IMAGE_DIR = str(os.environ.get('UPLOAD_IMAGE_DIR'))
+
+    UPLOAD_DOC_EXTENSIONS = list(str(os.environ.get('UPLOAD_DOC_EXTENSIONS')).split(' '))
+    UPLOAD_DOC_PATH = str(os.environ.get('UPLOAD_DOC_PATH'))
+    UPLOAD_DOC_DIR = str(os.environ.get('UPLOAD_DOC_DIR'))
 
     MAIL_SERVER = str(os.environ.get('MAIL_SERVER'))
     MAIL_PORT = str(os.environ.get('MAIL_PORT'))
