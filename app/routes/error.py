2ca1637727724614cf9af13393b73fa98cfa8463
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-05-27 05:43:34 +0700

    refactor: pass error message instead of creating new file

--- app/routes/error.py
+++ app/routes/error.py
@@ -4,4 +4,9 @@ error = Blueprint('error', __name__, template_folder='templates')
 
 @error.route('/unauthorized', methods=['GET'])
 def unauthorized():
-  return render_template('error/401.html')
+  return render_template('error/error.html', error_msg="401 | Unauthorized")
+
+
+@error.route('/bad-request', methods=['GET'])
+def bad_request():
+  return render_template('error/error.html', error_msg="400 | Bad Request")
